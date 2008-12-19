7f51ca5c5251d44b194b2b43e2481d682da261fa
joshuaflanagan <joshuaflanagan@gmail.com>
2008-12-19 15:17:56 +0000

    Added missing DebuggerVisualizers test project

---
+++ Source/StructureMap.DebuggerVisualizers.Testing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace StructureMap.DebuggerVisualizers.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new Container( x =>
+                                               {
+                                                   x.Scan(s =>
+                                                              {
+                                                                  s.TheCallingAssembly();
+                                                                  s.WithDefaultConventions();
+                                                              });
+                                                   x.ForRequestedType<IDoMore<string>>().TheDefaultIsConcreteType<DoForStrings>();
+                                                   x.ForRequestedType<IDoThat>().AddInstances(i =>
+                                                                                                  {
+                                                                                                      i.OfConcreteType<DoThat>().WithName("Red");
+                                                                                                      i.OfConcreteType<DoThat>().WithName("Blue");
+                                                                                                  });
+                                               });
+
+            ObjectFactory.Initialize(i => i.ForRequestedType<IDoThat>().TheDefaultIsConcreteType<DoThat>());
+            Debug.WriteLine(container.WhatDoIHave());
+
+            var details = ContainerVisualizerObjectSource.BuildContainerDetails(container);
+            Application.Run(new ContainerForm(details));
+        }
+    }
+}
---
+++ Source/StructureMap.DebuggerVisualizers.Testing/SampleTypes.cs
@@ -0,0 +1,63 @@
+﻿namespace StructureMap.DebuggerVisualizers.Testing
+{
+    public interface IDoThis
+    {
+        void Do();
+    }
+
+    public interface IDoThat
+    {
+        void DoIt();
+    }
+
+    public interface IDoMore<T>
+    {
+        void DoDo(T thing);
+    }
+
+    public interface IHasTwoGenerics<FIRST, SECOND>
+    {
+        SECOND DoIt(FIRST input);
+    }
+
+    public class DoThis : IDoThis
+    {
+        public void Do()
+        {
+            
+        }
+    }
+
+    public class DoThat : IDoThat
+    {
+        public void DoIt()
+        {
+            
+        }
+    }
+
+    public class DoMore<T> :IDoMore<T>
+    {
+        public void DoDo(T thing)
+        {
+            
+        }
+    }
+
+    public class HasTwoGenerics<FIRST, SECOND> : IHasTwoGenerics<FIRST, SECOND>
+    {
+        public SECOND DoIt(FIRST input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class DoForStrings :IDoMore<string>
+    {
+        public void DoDo(string thing)
+        {
+            
+
+        }
+    }
+}
