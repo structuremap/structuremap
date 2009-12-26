bdc05097ce9b8a3b7106560438a32aa3c5c1be1e
jeremydmiller <jeremydmiller@yahoo.com>
2009-12-26 05:32:47 +0000

    The big, big, big switch from IL generation to dynamic Func's.  Ding dong, the witch is dead.

--- Source/StructureMap.Testing.Widget2/EnumerationCheck.cs
+++ Source/StructureMap.Testing.Widget2/EnumerationCheck.cs
@@ -26,63 +26,6 @@ namespace StructureMap.Testing.Widget2
     }
 
 
-    public class CowBuilder : InstanceBuilder
-    {
-        public Type ThePluginType
-        {
-            get
-            {
-                return typeof (Cow);
-            }
-        }
-
-        public override Type PluggedType
-        {
-            get { return typeof (Cow); }
-        }
-
-
-        public override object BuildInstance(IConfiguredInstance instance, BuildSession session)
-        {
-            return new Cow(
-                long.Parse(instance.GetProperty("Weight")),
-                (BreedEnum) Enum.Parse(typeof (BreedEnum), instance.GetProperty("Breed"), true),
-                instance.GetProperty("Name"));
-        }
-    }
-
-    public class SetterBuilder : InstanceBuilder
-    {
-        public override Type PluggedType
-        {
-            get { throw new System.NotImplementedException(); }
-        }
-
-        public override object BuildInstance(IConfiguredInstance instance, BuildSession session)
-        {
-            SetterTarget target = new SetterTarget();
-
-
-            //instance.ForProperty("Name", s => );
-            //target.Name = instance.GetProperty("Name");
-            //if (instance.HasProperty("Name")) target.Name = instance.GetProperty("Name");
-            //if (instance.HasProperty("Name2")) target.Name = instance.GetProperty("Name2");
-            //if (instance.HasProperty("Age")) target.Age = (int)Convert.ChangeType(instance.GetProperty("Age"), typeof(int));
-            //if (instance.HasProperty("Breed")) target.Breed = (BreedEnum)Enum.Parse(typeof(BreedEnum), instance.GetProperty("Breed"), true);
-
-            //instance.ForProperty("Name", x => target.Name = x);
-            //instance.ForProperty("Age", x => target.Age = (int)Convert.ChangeType(x, typeof (int)));
-            //instance.ForProperty("Breed", x => target.Breed = (BreedEnum) Enum.Parse(typeof (BreedEnum), x));
-
-            return target;
-        }
-
-        private void setNameProperty(SetterTarget target, string text)
-        {
-            target.Name = text;
-        }
-    }
-
     public class SetterTarget
     {
         public string Name { get; set; }
