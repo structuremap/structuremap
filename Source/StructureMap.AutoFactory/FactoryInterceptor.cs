ee0b9242666204e8a1052e30bdf3564f97a023f7
tkellogg <timothy.kellogg@gmail.com>
2012-05-22 21:07:12 -0600

    Cleaned up auto-factory parameters
    Now it works for many parameters, and added tests to verify some other
    scenarios

--- Source/StructureMap.AutoFactory/FactoryInterceptor.cs
+++ Source/StructureMap.AutoFactory/FactoryInterceptor.cs
@@ -6,6 +6,12 @@ namespace StructureMap.AutoFactory
     public class FactoryInterceptor : IInterceptor
     {
         private readonly IContext _context;
+        private IContainer _container;
+
+        private IContainer Container
+        {
+            get { return _container ?? (_container = _context.GetInstance<IContainer>()); }
+        }
 
         public FactoryInterceptor(IContext context)
         {
@@ -16,42 +22,50 @@ namespace StructureMap.AutoFactory
         {
             Type pluginType;
 
-        	int remainingArguments = invocation.Arguments.Length;
+            int remainingArguments = invocation.Arguments.Length;
             if ((invocation.Arguments.Length > 0) && (invocation.Arguments[0] is Type))
             {
                 pluginType = (Type) invocation.Arguments[0];
-            	remainingArguments--;
+                remainingArguments--;
             }
             else
             {
                 pluginType = invocation.Method.ReturnType;                
             }
 
-        	var returnValue = GetReturnValue(invocation, pluginType, remainingArguments);
-        	invocation.ReturnValue = returnValue;
+            var returnValue = GetReturnValue(invocation, pluginType, remainingArguments);
+            invocation.ReturnValue = returnValue;
         }
 
-    	private object GetReturnValue(IInvocation invocation, Type pluginType, int remainingArguments)
-    	{
-    		object returnValue;
-    		if (remainingArguments > 0)
-    		{
-    			var container = _context.GetInstance<IContainer>();
-    			var i = invocation.Arguments.Length - remainingArguments;
-    			returnValue = GetReturnValueForMultipleArguments(invocation, pluginType, i, container);
-    		}
-    		else
-    			returnValue = _context.GetInstance(pluginType);
-    		return returnValue;
-    	}
+        private object GetReturnValue(IInvocation invocation, Type pluginType, int remainingArguments)
+        {
+            object returnValue;
+            if (remainingArguments > 0)
+            {
+                var i = invocation.Arguments.Length - remainingArguments;
+                returnValue = GetReturnValueForMultipleArguments(invocation, pluginType, i, Container);
+            }
+            else
+                returnValue = _context.GetInstance(pluginType);
+            return returnValue;
+        }
 
-    	private static object GetReturnValueForMultipleArguments(IInvocation invocation, Type pluginType, int i,
-    	                                                         IContainer container)
-    	{
-    		var parameters = invocation.Method.GetParameters();
-    		var name = parameters[i].Name;
-    		var value = invocation.Arguments[i];
-    		return container.With(name).EqualTo(value).GetInstance(pluginType);
-    	}
+        private static object GetReturnValueForMultipleArguments(IInvocation invocation, Type pluginType, int i,
+                                                                 IContainer container)
+        {
+            var parameters = invocation.Method.GetParameters();
+            var name = parameters[i].Name;
+            var value = invocation.Arguments[i];
+            var expr = container.With(name).EqualTo(value);
+
+            for (int k = i + 1; k < invocation.Arguments.Length; k++)
+            {
+                name = parameters[k].Name;
+                value = invocation.Arguments[k];
+                expr = expr.With(name).EqualTo(value);
+            }
+
+            return expr.GetInstance(pluginType);
+        }
     }
 }
--- Source/StructureMap.Testing/AutoFactories/ProxyFactoryTests.cs
+++ Source/StructureMap.Testing/AutoFactories/ProxyFactoryTests.cs
@@ -59,35 +59,92 @@ namespace StructureMap.Testing.AutoFactories
             built.ShouldBeTheSameAs(service);
         }
 
-		[TestFixture]
-		public class WhenUsingExtraParameters
-		{
-			#region Types used for testing
-			public class SingleParameter
-			{
-				public string Name;
-				public SingleParameter(string name)
-				{
-					Name = name;
-				}
-			}
+        [TestFixture]
+        public class WhenUsingExtraParameters
+        {
+            #region Types used for testing
+            public class SingleParameter
+            {
+                public string Name;
+                public SingleParameter(string name)
+                {
+                    Name = name;
+                }
+            }
 
-			public interface IFactory
-			{
-				SingleParameter Get(string name);
-			}
-			#endregion
+            public class SeveralPrimitives
+            {
+                public readonly int TheAnswer;
+                public readonly bool IsTrue;
 
-			[Test]
-			public void It_uses_the_name_of_the_parameter_as_a_key()
-			{
-				using (var container = new Container(cfg => cfg.For<IFactory>().CreateFactory()))
-				{
-					var factory = container.GetInstance<IFactory>();
-					var value = factory.Get("foo");
-					Assert.AreEqual("foo", value.Name);
-				}
-			}
-		}
+                public SeveralPrimitives(int theAnswer, bool isTrue)
+                {
+                    TheAnswer = theAnswer;
+                    IsTrue = isTrue;
+                }
+            }
+
+            public class SeveralPrimitivesAndService
+            {
+                public readonly int TheAnswer;
+                public readonly bool IsTrue;
+                private readonly IService service;
+
+                public SeveralPrimitivesAndService(int theAnswer, bool isTrue, IService service)
+                {
+                    TheAnswer = theAnswer;
+                    IsTrue = isTrue;
+                    this.service = service;
+                }
+            }
+
+            public interface IFactory
+            {
+                SingleParameter GetSingleParameter(string name);
+                SeveralPrimitives GetSeveralPrimitives(int theAnswer, bool isTrue);
+                SeveralPrimitivesAndService GetSeveralPrimitivesAndService(int theAnswer, bool isTrue);
+            }
+            #endregion
+
+            [Test]
+            public void It_uses_the_name_of_the_parameter_as_a_key()
+            {
+                using (var container = new Container(cfg => cfg.For<IFactory>().CreateFactory()))
+                {
+                    var factory = container.GetInstance<IFactory>();
+                    var value = factory.GetSingleParameter("foo");
+                    Assert.AreEqual("foo", value.Name);
+                }
+            }
+
+            [Test]
+            public void It_can_do_several_parameters()
+            {
+                using (var container = new Container(cfg => cfg.For<IFactory>().CreateFactory()))
+                {
+                    var factory = container.GetInstance<IFactory>();
+                    var value = factory.GetSeveralPrimitives(42, true);
+                    Assert.AreEqual(42, value.TheAnswer);
+                    Assert.AreEqual(true, value.IsTrue);
+                }
+            }
+
+            [Test]
+            public void It_can_still_resolve_regular_pluginTypes_with_parameters()
+            {
+                var container = new Container(cfg =>
+                                                {
+                                                    cfg.For<IFactory>().CreateFactory();
+                                                    cfg.For<IService>().Use<Service>();
+                                                });
+                using (container)
+                {
+                    var factory = container.GetInstance<IFactory>();
+                    var value = factory.GetSeveralPrimitives(42, true);
+                    Assert.AreEqual(42, value.TheAnswer);
+                    Assert.AreEqual(true, value.IsTrue);
+                }
+            }
+        }
     }
 }
