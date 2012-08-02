a9974656290767d652fbbfa48fa26e53249eea47
greg <greg.nehus@gmail.com>
2012-08-02 19:50:43 -0400

    added nsubstitute automocking

---
+++ Source/StructureMap.AutoMocking/NSubstituteFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace StructureMap.AutoMocking
+{
+    public class SubstituteFactory
+    {
+        private readonly Func<Type, object> _factory;
+
+        public SubstituteFactory()
+        {
+            var assembly = Assembly.Load("NSubstitute");
+            var type = assembly.GetType("NSubstitute.Substitute");
+            
+            if (type == null)
+                throw new InvalidOperationException("Can't find Substitute class in assembly @ " + assembly.Location);
+
+            var method = type.GetMethods().First(x => x.ContainsGenericParameters && x.GetGenericArguments().Length == 1);
+
+            _factory = typeToMock => method.MakeGenericMethod(typeToMock).Invoke(null, new object[1]{null});
+        }
+
+        public object CreateMock(Type type)
+        {
+            return _factory(type);
+        }
+    }
+}
---
+++ Source/StructureMap.AutoMocking/NSubstituteServiceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StructureMap.AutoMocking
+{
+    public class NSubstituteServiceLocator : ServiceLocator
+    {
+        private readonly SubstituteFactory _substituteFactory = new SubstituteFactory();
+
+        public T Service<T>() where T : class
+        {
+            return (T)_substituteFactory.CreateMock(typeof(T));
+        }
+
+        public object Service(Type serviceType)
+        {
+            return _substituteFactory.CreateMock(serviceType);
+        }
+
+        public T PartialMock<T>(params object[] args) where T : class
+        {
+            return (T)_substituteFactory.CreateMock(typeof(T));
+        }
+    }
+}
