da79cd2b3a316b329243a364050882541d6a4d11
jeremydmiller <jeremydmiller@yahoo.com>
2012-11-04 12:03:02 -0600

    reving buildsupport

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,11 +1,10 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyDescription("2.6.4.51049")]
+[assembly: AssemblyDescription("2.6.4.51202")]
 [assembly: AssemblyProduct("StructureMap")]
 [assembly: AssemblyCopyright("Copyright 2012 Jeremy D. Miller, Joshua Flanagan, Tim Kellogg. All rights reserved.")]
-[assembly: AssemblyTrademark("cbcc9dbef0c5d5243470aca7c48c0841ae43987e")]
+[assembly: AssemblyTrademark("c66119ec6542f77f0c35dc7e7e17f659ac67fb92")]
 [assembly: AssemblyVersion("2.6.4.0")]
-[assembly: AssemblyFileVersion("2.6.4.51049")]
+[assembly: AssemblyFileVersion("2.6.4.51202")]
 
 [assembly: AssemblyInformationalVersion("2.6.4.0")]
-
