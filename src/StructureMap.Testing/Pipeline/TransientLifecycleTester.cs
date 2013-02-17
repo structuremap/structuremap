a159857db5d8df372662f0b577a6ff3ffb34e4fd
jeremydmiller <jeremydmiller@yahoo.com>
2013-02-17 13:36:13 -0600

    added TransientLifecycle

---
+++ src/StructureMap.Testing/Pipeline/TransientLifecycleTester.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using StructureMap.Pipeline;
+
+namespace StructureMap.Testing.Pipeline
+{
+    [TestFixture]
+    public class TransientLifecycleTester
+    {
+        private ILifecycleContext theContext;
+        private TransientLifecycle theLifecycle;
+        private IObjectCache theCache;
+
+        [SetUp]
+        public void SetUp()
+        {
+            theContext = MockRepository.GenerateMock<ILifecycleContext>();
+            theLifecycle = new TransientLifecycle();
+
+            theCache = MockRepository.GenerateMock<IObjectCache>();
+            theContext.Stub(x => x.Transients).Return(theCache);
+        }
+
+        [Test]
+        public void the_cache_is_from_the_transient_of_the_context()
+        {
+            theLifecycle.FindCache(theContext).ShouldBeTheSameAs(theCache);
+        }
+
+        [Test]
+        public void eject_all_delegates()
+        {
+            theLifecycle.EjectAll(theContext);
+
+            theCache.AssertWasCalled(x => x.DisposeAndClear());
+        }
+
+    }
+}
--- src/StructureMap/Pipeline/ILifecycle.cs
+++ src/StructureMap/Pipeline/ILifecycle.cs
@@ -6,4 +6,22 @@ namespace StructureMap.Pipeline
         void EjectAll(ILifecycleContext context);
         IObjectCache FindCache(ILifecycleContext context);
     }
+
+    public class TransientLifecycle : ILifecycle
+    {
+        public string Scope
+        {
+            get { return InstanceScope.Transient.ToString(); }
+        }
+
+        public void EjectAll(ILifecycleContext context)
+        {
+            FindCache(context).DisposeAndClear();
+        }
+
+        public IObjectCache FindCache(ILifecycleContext context)
+        {
+            return context.Transients;
+        }
+    }
 }
