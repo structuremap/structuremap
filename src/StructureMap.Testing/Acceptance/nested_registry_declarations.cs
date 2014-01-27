1ee29f27b2f33c00be994c0551358f6d46f7f872
jeremydmiller <jeremydmiller@yahoo.com>
2014-01-27 15:33:36 -0600

    added a new acceptance test for nested registries.  Closes GH-161

---
+++ src/StructureMap.Testing/Acceptance/nested_registry_declarations.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using StructureMap.Configuration.DSL;
+
+namespace StructureMap.Testing.Acceptance
+{
+    [TestFixture]
+    public class nested_registry_declarations
+    {
+        [Test]
+        public void get_registrations_from_all_included_registries()
+        {
+            var container = new Container(new ARegistry());
+
+            container.GetAllInstances<IWidget>().OrderBy(x => x.GetType().Name)
+                .Select(x => x.GetType())
+                .ShouldHaveTheSameElementsAs(typeof(AWidget), typeof(BWidget), typeof(CWidget), typeof(DefaultWidget));
+        }
+
+        [Test]
+        public void get_registrations_from_all_included_registries_through_configure()
+        {
+            var container = new Container();
+            container.Configure(x => x.IncludeRegistry<ARegistry>());
+
+            container.GetAllInstances<IWidget>().OrderBy(x => x.GetType().Name)
+                .Select(x => x.GetType())
+                .ShouldHaveTheSameElementsAs(typeof(AWidget), typeof(BWidget), typeof(CWidget), typeof(DefaultWidget));
+        }
+    }
+
+    public class ARegistry : Registry
+    {
+        public ARegistry()
+        {
+            For<IWidget>().Add<AWidget>();
+
+            IncludeRegistry<BRegistry>();
+            IncludeRegistry<CRegistry>();
+        }
+    }
+
+    public class BRegistry : Registry
+    {
+        public BRegistry()
+        {
+            For<IWidget>().Add<BWidget>();
+
+            IncludeRegistry<DefaultRegistry>();
+        }
+    }
+
+    public class CRegistry : Registry
+    {
+        public CRegistry()
+        {
+            For<IWidget>().Add<CWidget>();
+        }
+    }
+
+    public class DefaultRegistry : Registry
+    {
+        public DefaultRegistry()
+        {
+            For<IWidget>().Add<DefaultWidget>();
+        }
+    }
+}
