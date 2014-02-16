c7f14018e54065072a71157b3711b0069312ef7d
J Iaccarino <reiv3r@hush.com>
2014-02-15 22:59:33 -0800

    Added ability to add all implementations on a type to the registry.

--- src/StructureMap/Graph/AssemblyScanner.cs
+++ src/StructureMap/Graph/AssemblyScanner.cs
@@ -73,6 +73,13 @@ namespace StructureMap.Graph
             return filter;
         }
 
+        public FindAllTypeImplementationsFilter AddAllTypeImplementationsOf(Type pluginType)
+        {
+            var filter = new FindAllTypeImplementationsFilter(pluginType);
+            With(filter);
+
+            return filter;
+        }
 
         public void Exclude(Func<Type, bool> exclude)
         {
