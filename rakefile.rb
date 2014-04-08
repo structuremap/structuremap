df606dbc94ea8a0dc603105c350a5a1db8a4f722
jeremydmiller <jeremydmiller@yahoo.com>
2014-04-08 18:48:26 -0500

    turning off the WP8 profile in nuget publishing until we can figure out what's up on the build server

--- rakefile.rb
+++ rakefile.rb
@@ -11,10 +11,10 @@ FubuRake::Solution.new do |sln|
 		:copyright => 'Copyright 2004-2014 Jeremy D. Miller, Joshua Flanagan, Frank Quednau, Tim Kellogg, et al. All rights reserved.'
 	}
 	
-	sln.compile_targets = ['Debug', 'Release', 'NET45WP8']
+	#sln.compile_targets = ['Debug', 'Release', 'NET45WP8']
 	
 	sln.ripple_enabled = true
 	sln.fubudocs_enabled = true
 	
-	sln.ci_steps = ['compile:net45wp8']
+	#sln.ci_steps = ['compile:net45wp8']
 end
