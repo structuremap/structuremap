dac60bf001cbc37660595aae0e3fcdf379c3c2c9
jeremydmiller <jeremydmiller@yahoo.com>
2014-10-06 20:11:11 -0500

    added an example of a generic builder

---
+++ src/StructureMap.Testing/Acceptance/builder_for_open_generic_type.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using StructureMap.Building;
+using StructureMap.Pipeline;
+
+namespace StructureMap.Testing.Acceptance
+{
+    [TestFixture]
+    public class builder_for_open_generic_type
+    {
+        [Test]
+        public void show_the_workaround_for_generic_builders()
+        {
+            var container = new Container(_ => {
+                _.For(typeof (IRepository<,>)).Use(new RepositoryInstanceFactory());
+            });
+
+            container.GetInstance<IRepository<string, int>>()
+                .ShouldBeOfType<Repository<string, int>>();
+        }
+    }
+
+    public class RepositoryInstanceFactory : Instance
+    {
+        public override Instance CloseType(Type[] types)
+        {
+            // StructureMap will cache the object built out of this,
+            // so the expensive Reflection hit only happens
+            // once
+            var instanceType = typeof (RepositoryInstance<,>).MakeGenericType(types);
+            return Activator.CreateInstance(instanceType).As<Instance>();
+        }
+
+        public override IDependencySource ToDependencySource(Type pluginType)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override string Description
+        {
+            get { return "Build Repository<T, T1>() with RepositoryBuilder"; }
+        }
+
+        public override Type ReturnedType
+        {
+            get { return typeof(Repository<,>); }
+        }
+    }
+
+    public class RepositoryInstance<T, T1> : LambdaInstance<IRepository<T, T1>>
+    {
+        public RepositoryInstance() : base(() => RepositoryBuilder.Build<T, T1>())
+        {
+        }
+    }
+
+    public static class RepositoryBuilder
+    {
+        public static IRepository<T, T1> Build<T, T1>()
+        {
+            return new Repository<T, T1>();
+        } 
+    }
+
+    public interface IRepository<T, T1>{}
+    public class Repository<T, T1> : IRepository<T, T1>{} 
+}
