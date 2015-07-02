6a7fb24511d8d8fc5d16f5492d4c4205aa03be05
jeremydmiller <jeremydmiller@yahoo.com>
2015-07-02 16:01:53 -0500

    repro steps for Bug 338

---
+++ src/StructureMap.Testing/Bugs/Bug_338.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+using StructureMap.Graph;
+
+namespace StructureMap.Testing.Bugs
+{
+    [TestFixture]
+    public class Bug_338
+    {
+        public abstract class ParentClass
+        {
+        }
+
+        public class ChildClass : ParentClass
+        {
+        }
+
+        [Test]
+        public void be_smart_and_do_not_add_abstract_types()
+        {
+            var container = new Container(_ =>
+            {
+                _.Scan(x =>
+                {
+                    x.TheCallingAssembly();
+                    x.AddAllTypesOf<ParentClass>();
+                });
+            });
+
+            container.GetAllInstances<ParentClass>().Count()
+                .ShouldBe(1);
+        }
+    }
+}
