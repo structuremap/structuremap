8ec03abefdaa7bf038a50b030fd5d77bf99fc733
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-07-02 10:18:55 -0500

    Adding CommonAssemblyInfo to the git repo. Will regret that later.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyDescription("3.1.5.51154")]
+[assembly: AssemblyProduct("StructureMap")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright 2004-2014 Jeremy D. Miller, Joshua Flanagan, Frank Quednau, Tim Kellogg, et al. All rights reserved."
+        )]
+[assembly: AssemblyTrademark("0f77ee9e6ab9d3f1ce734fc6bda6e6611cf94737")]
+[assembly: AssemblyVersion("3.1.5.0")]
+[assembly: AssemblyFileVersion("3.1.5.51154")]
+[assembly: AssemblyInformationalVersion("3.1.5.0")]
