772849d903d2671019aca7764f616ccbf0a6dc2e
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-07-02 10:33:51 -0500

    Added a :version task to rake. Cleaned out unnecessary files

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,12 @@
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "results"
+BUILD_VERSION = '3.1.6'
+
+tc_build_number = ENV["BUILD_NUMBER"]
+build_revision = tc_build_number || Time.new.strftime('5%H%M')
+build_number = "#{BUILD_VERSION}.#{build_revision}"
+BUILD_NUMBER = build_number 
+
 
 task :default => [:test]
 
@@ -10,7 +17,44 @@ task :clean do
 
 end
 
-task :compile => [:clean] do
+desc "Update the version information for the build"
+task :version do
+  asm_version = build_number
+  
+  begin
+    commit = `git log -1 --pretty=format:%H`
+  rescue
+    commit = "git unavailable"
+  end
+  puts "##teamcity[buildNumber '#{build_number}']" unless tc_build_number.nil?
+  puts "Version: #{build_number}" if tc_build_number.nil?
+  
+  options = {
+	:description => 'IoC Container for .Net',
+	:product_name => 'StructureMap',
+	:copyright => 'Copyright 2004-2015 Jeremy D. Miller, Joshua Flanagan, Frank Quednau, Tim Kellogg, et al. All rights reserved.',
+	:trademark => commit,
+	:version => asm_version,
+	:file_version => build_number,
+	:informational_version => asm_version
+	
+  }
+  
+  puts "Writing src/CommonAssemblyInfo.cs..."
+	File.open('src/CommonAssemblyInfo.cs', 'w') do |file|
+		file.write "using System.Reflection;\n"
+		file.write "using System.Runtime.InteropServices;\n"
+		file.write "[assembly: AssemblyDescription(\"#{options[:description]}\")]\n"
+		file.write "[assembly: AssemblyProduct(\"#{options[:product_name]}\")]\n"
+		file.write "[assembly: AssemblyCopyright(\"#{options[:copyright]}\")]\n"
+		file.write "[assembly: AssemblyTrademark(\"#{options[:trademark]}\")]\n"
+		file.write "[assembly: AssemblyVersion(\"#{options[:version]}\")]\n"
+		file.write "[assembly: AssemblyFileVersion(\"#{options[:file_version]}\")]\n"
+		file.write "[assembly: AssemblyInformationalVersion(\"#{options[:informational_version]}\")]\n"
+	end
+end
+
+task :compile => [:clean, :version] do
 	sh "C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe src/StructureMap.sln   /property:Configuration=#{COMPILE_TARGET} /v:m /t:rebuild /nr:False /maxcpucount:2"
 end
 
--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,12 +1,9 @@
 using System.Reflection;
-
-[assembly: AssemblyDescription("3.1.5.51154")]
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("IoC Container for .Net")]
 [assembly: AssemblyProduct("StructureMap")]
-[assembly:
-    AssemblyCopyright(
-        "Copyright 2004-2014 Jeremy D. Miller, Joshua Flanagan, Frank Quednau, Tim Kellogg, et al. All rights reserved."
-        )]
-[assembly: AssemblyTrademark("0f77ee9e6ab9d3f1ce734fc6bda6e6611cf94737")]
-[assembly: AssemblyVersion("3.1.5.0")]
-[assembly: AssemblyFileVersion("3.1.5.51154")]
-[assembly: AssemblyInformationalVersion("3.1.5.0")]
+[assembly: AssemblyCopyright("Copyright 2004-2014 Jeremy D. Miller, Joshua Flanagan, Frank Quednau, Tim Kellogg, et al. All rights reserved.")]
+[assembly: AssemblyTrademark("27b1f5e7f7caaba82e616811f4c5a91d9a633205")]
+[assembly: AssemblyVersion("3.1.6.51031")]
+[assembly: AssemblyFileVersion("3.1.6.51031")]
+[assembly: AssemblyInformationalVersion("3.1.6.51031")]
