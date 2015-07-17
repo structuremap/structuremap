cd303b2c8103759c713f94f50e900d093aea5434
jeremydmiller <jeremydmiller@yahoo.com>
2015-07-17 10:08:29 -0500

    Documentation Update

---
+++ publish-docs.cmd
@@ -0,0 +1,16 @@
+
+src\ST\bin\debug\ST.exe doc-export z:\code\structuremap.github.com Website
+
+
+cd \structuremap.github.com
+
+
+git add --all
+
+
+
+git commit -a -m "Documentation Update"
+
+
+
+git push origin master 
