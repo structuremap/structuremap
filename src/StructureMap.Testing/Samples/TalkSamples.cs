6b949e373a5c46d403d9caa23eb682fa493613ec
jeremydmiller <jeremydmiller@yahoo.com>
2015-09-22 12:41:17 -0500

    new sample for the long lived codebase talk

---
+++ src/StructureMap.Testing/Samples/TalkSamples.cs
@@ -0,0 +1,67 @@
+﻿using Shouldly;
+using StructureMap.Configuration.DSL;
+using StructureMap.Graph;
+
+namespace StructureMap.Testing.Samples
+{
+    public interface IVisualizer<T>
+    {
+        string Visualize(T target);
+    }
+
+    public class BasicVisualizer<T> : IVisualizer<T>
+    {
+        public string Visualize(T target)
+        {
+            return target.ToString();
+        }
+    }
+
+    public class SpecialLog { }
+
+    public class SpecialLogVisualizer : IVisualizer<SpecialLog>
+    {
+        public string Visualize(SpecialLog target)
+        {
+            return "Special HTML Generation";
+        }
+    }
+
+    public class GenericSamples
+    {
+        public void BuildContainer()
+        {
+            var container = new Container(_ =>
+            {
+                _.For(typeof (IVisualizer<>)).Use(typeof (BasicVisualizer<>));
+
+                _.For<IVisualizer<SpecialLog>>().Use<SpecialLogVisualizer>();
+                // Or conventionally -->
+                _.Scan(scan =>
+                {
+                    scan.TheCallingAssembly();
+                    scan.ConnectImplementationsToTypesClosing(typeof (IVisualizer<>));
+                });
+            });
+
+            container.GetInstance<IVisualizer<SpecialLog>>()
+                .ShouldBeOfType<SpecialLogVisualizer>();
+        }
+    }
+
+
+    public interface ISession { }
+
+    public interface ISessionFactory
+    {
+        ISession Build();
+    }
+
+    public class LambdaRegistry : Registry
+    {
+        public LambdaRegistry()
+        {
+            For<ISession>().Use(c => c.GetInstance<ISessionFactory>().Build());
+        }
+    }
+}
