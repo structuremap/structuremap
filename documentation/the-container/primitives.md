8a4fedf3648f4e0356ebd0899dea39f092148288
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-10-08 09:58:08 -0500

    Docs on using primitive types. Closes GH-398

--- documentation/the-container/primitives.md
+++ documentation/the-container/primitives.md
@@ -1,1 +1,36 @@
 <!--Title:Working with Primitive Types-->
+
+StructureMap treats simple types like strings, numbers of any kind, enumerations, and dates as _primitive_
+types that are completely exempt from <[linkto:the-container/auto-wiring;title=auto wiring]> -- meaning that any 
+constructor or setter dependencies on these types must be supplied as <[linkto:registration/inline-dependencies;title=inline dependencies]>.
+
+To make this concrete, if you ask StructureMap to build a concrete type that has dependencies on simple types without
+like this example, StructureMap will throw an exception telling you that it cannot build the instance:
+
+<[sample:GuyWithNoDefaultName]>
+
+Part of the exception message thrown in the unit test shown above is the erroneous <[linkto:diagnostics/build-plans;title=build plan]>
+showing you that the `name` parameter has to be defined:
+
+<pre>
+	new GuyWithNoDefaultName(String name)
+	  ┗ String name = Required primitive dependency is not explicitly defined
+</pre>
+
+We can build `GuyWithNoDefaultName` by supplying a value for `name` as I did in the following
+sample:
+
+<[sample:GuyWithNoDefaultName-explicit-argument]>
+
+See <[linkto:registration/policies]> for an example of using a constructor policy to set a dependency
+on a "connectionString" argument in a conventional way.
+
+## Default Values
+
+As a new feature in the 4.0 release, StructureMap can finally take advantage of default parameter arguments to
+derive the values for a primitive argument (or setter value) while still allowing you to explicitly define
+that parameter or setter value:
+
+<[sample:GuyWithName-defaults]>
+
+
--- src/StructureMap.Testing/using_default_values_on_constructor_function_if_no_explicit_value.cs
+++ src/StructureMap.Testing/using_default_values_on_constructor_function_if_no_explicit_value.cs
@@ -6,11 +6,26 @@ namespace StructureMap.Testing
     [TestFixture]
     public class using_default_values_on_constructor_function_if_no_explicit_value
     {
+        // SAMPLE: GuyWithName-defaults
+        // I was listening to Jim Croce's "I've got a Name" song
+        // when I wrote this feature;)
+        public class GuyWithName
+        {
+            public GuyWithName(string name = "Jim Croce")
+            {
+                Name = name;
+            }
+
+            public string Name { get; set; }
+        }
+
         [Test]
         public void uses_the_default_value_if_one_exists()
         {
             var container = new Container();
 
+            // Should happily build with the default
+            // value of 'name'
             container.GetInstance<GuyWithName>()
                 .Name.ShouldBe("Jim Croce");
         }
@@ -23,6 +38,8 @@ namespace StructureMap.Testing
                 _.ForConcreteType<GuyWithName>();
             });
 
+            // Should happily build with the default
+            // value of 'name'
             container.GetInstance<GuyWithName>()
                 .Name.ShouldBe("Jim Croce");
         }
@@ -32,21 +49,54 @@ namespace StructureMap.Testing
         {
             var container = new Container(_ =>
             {
-                _.ForConcreteType<GuyWithName>().Configure.Ctor<string>("name").Is("Eric Clapton");
+                _.ForConcreteType<GuyWithName>()
+                    .Configure.Ctor<string>("name").Is("Eric Clapton");
             });
 
             container.GetInstance<GuyWithName>()
                 .Name.ShouldBe("Eric Clapton");
         }
+        // ENDSAMPLE
 
-        public class GuyWithName
+
+
+
+        // SAMPLE: GuyWithNoDefaultName
+        public class GuyWithNoDefaultName
         {
-            public GuyWithName(string name = "Jim Croce")
+            // StructureMap will not use any kind of auto-wiring
+            // on name
+            public GuyWithNoDefaultName(string name)
             {
-                Name = name;
             }
+        }
 
-            public string Name { get; set; }
-        }    
+        [Test]
+        public void cannot_build_simple_arguments()
+        {
+            var container = new Container();
+
+            Exception<StructureMapBuildPlanException>.ShouldBeThrownBy(() =>
+            {
+                container.GetInstance<GuyWithNoDefaultName>();
+            });
+        }
+        // ENDSAMPLE
+
+        // SAMPLE: GuyWithNoDefaultName-explicit-argument
+        [Test]
+        public void can_build_with_explicit_argument()
+        {
+            var container = new Container(_ =>
+            {
+                _.ForConcreteType<GuyWithNoDefaultName>()
+                    .Configure.Ctor<string>("name").Is("Steve Winwood");
+            });
+
+            container.GetInstance<GuyWithNoDefaultName>()
+                .ShouldNotBeNull();
+        }
+        // ENDSAMPLE
+        
     }
 }
