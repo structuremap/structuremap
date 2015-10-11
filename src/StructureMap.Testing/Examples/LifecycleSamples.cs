772d8a8ac5512642c52a35ace015fec33c21d8e5
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-10-11 14:04:21 -0500

    lifecycle sample

---
+++ src/StructureMap.Testing/Examples/LifecycleSamples.cs
@@ -0,0 +1,47 @@
+﻿namespace StructureMap.Testing.Examples
+{
+    // SAMPLE: evil-singleton
+    public class EvilSingleton
+    {
+        public static readonly EvilSingleton Instance = new EvilSingleton();
+
+        private EvilSingleton()
+        {
+        }
+
+        public void DoSomething()
+        {
+            // do something with the static data here
+        }
+    }
+
+
+    public class EvilSingletonUser
+    {
+        public void DoWork()
+        {
+            EvilSingleton.Instance.DoSomething();
+        }
+    }
+
+    // ENDSAMPLE
+
+    // SAMPLE: no-singleton
+    public class SingletonService { }
+
+    public class SingletonUser
+    {
+        public SingletonUser(SingletonService singleton)
+        {
+        }
+    }
+
+    public class SingletonScopeRegistry : Registry
+    {
+        public SingletonScopeRegistry()
+        {
+            For<SingletonService>().Singleton();
+        }
+    }
+    // ENDSAMPLE
+}
