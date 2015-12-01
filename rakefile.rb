98c780f2822d103190c404dbee376070ab4ef1ba
jeremydmiller <jeremydmiller@yahoo.com>
2015-12-01 17:31:51 -0600

    last try before dinner

--- rakefile.rb
+++ rakefile.rb
@@ -14,7 +14,6 @@ task :default => [:test]
 desc "Do stupid Nuget stuff for idiotic .Net builds"
 task :csharp do
 	sh "nuget.exe install Microsoft.CSharp -Version 4.0.0"
-	Dir.mkdir "C:/Users/fubuadmin/.nuget/packages"
 	FileUtils.cp "Microsoft.CSharp.4.0.0/Microsoft.CSharp.4.0.0.nupkg", "C:/Users/fubuadmin/.nuget/packages"
 end
 
