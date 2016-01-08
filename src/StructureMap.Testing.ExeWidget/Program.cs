30a3a2ab403e48d496102bb47ca428aaa989fa74
Sigmund Lahn <sigmund@lahn.no>
2016-01-08 17:22:57 +0100

    Tests for includeExeFiles when scanning for assemblies

---
+++ src/StructureMap.Testing.ExeWidget/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StructureMap.Testing.ExeWidget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
