c313eb143ffa9aa22db7713e946c6fd4d5d50bd5
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-07-31 12:56:41 -0500

    test proving that [SetterProperty] works in CoreCLR from base classes. Closes GH-490

---
+++ src/StructureMap.Testing/Bugs/Bug_490_SetterProperty_in_CoreCLR_from_inherited_base.cs
@@ -0,0 +1,44 @@
+﻿using StructureMap.Attributes;
+using Xunit;
+
+namespace StructureMap.Testing.Bugs
+{
+    public class Bug_490_SetterProperty_in_CoreCLR_from_inherited_base
+    {
+        private Container container;
+
+        public Bug_490_SetterProperty_in_CoreCLR_from_inherited_base()
+        {
+            container = new Container(_ =>
+            {
+                _.For<IMyService>().Use<MyService>();
+                _.For<MyClass>().Use<MyClass>();
+            });
+        }
+
+        public interface IMyService { }
+
+        public class MyService : IMyService { }
+
+        public class BaseClass
+        {
+            [SetterProperty]
+            public IMyService MyServiceInBaseClass { get; set; }
+        }
+
+        public class MyClass : BaseClass
+        {
+            [SetterProperty]
+            public IMyService MyServiceInMyClass { get; set; }
+        }
+
+        [Fact]
+        public void property_should_be_injected_in_base_class()
+        {
+            var myClass = container.GetInstance<MyClass>();
+
+            myClass.MyServiceInBaseClass.ShouldNotBeNull();
+            myClass.MyServiceInMyClass.ShouldNotBeNull();
+        }
+    }
+}
