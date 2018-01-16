f21d12c21f462542315795dcef6c5ad0225e2e55
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-01-16 07:16:10 -0600

    bumping to 4.6

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@ require 'json'
 
 COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
 RESULTS_DIR = "results"
-BUILD_VERSION = '4.5.3'
+BUILD_VERSION = '4.6.0'
 
 DOC_LOCATION = ENV['docpath'].nil? ? "z:/code/structuremap.github.com" : ENV['docpath']
 
